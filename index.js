7b63a1f8f2a3dbf116b11272d8fb78ec7b5112a1
Rabby-sopno <sopno01737@gmail.com>
2020-10-12 20:51:07 +0600

    port double fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -14,7 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = 5000;
+let port = 5000;
 
 app.get("/", (req, res) => {
   res.send("hello world");
